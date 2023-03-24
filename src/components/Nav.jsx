63225ddeea3cfacac13a535b4d7ed4e4fb3a1aa5
wdavidcch <david.13.2121@gmail.com>
2023-03-24 13:57:57 -0400

    remove not useful mobile menu component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import MobileMenu from './MobileMenu';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -125,11 +124,91 @@ const Nav = () => {
           </li>
         </ul>
       </nav>
-      <MobileMenu
-        clicked={clicked}
-        handleClick={handleClick}
-        reference={menuRef}
-      />
+      <ul className={`links ${clicked ? 'active' : ''}`} ref={menuRef}>
+        <li className="nav-item close">
+          <button type="button" onClick={handleClick} onKeyDown={handleClick}>
+            <FontAwesomeIcon icon={faXmark} className="x-mark" />
+          </button>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="home"
+            onClick={handleClick}
+            spy
+            hashSpy
+            smooth
+            delay={200}
+            duration={600}
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            onClick={handleClick}
+            to="services"
+            spy
+            hashSpy
+            smooth
+            delay={200}
+            duration={600}
+          >
+            Services
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            onClick={handleClick}
+            to="about"
+            spy
+            hashSpy
+            smooth
+            delay={200}
+            duration={600}
+          >
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            onClick={handleClick}
+            to="portfolio"
+            spy
+            smooth
+            hashSpy
+            delay={200}
+            duration={600}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            onClick={handleClick}
+            to="skills"
+            spy
+            hashSpy
+            smooth
+            delay={200}
+            duration={600}
+          >
+            Skills
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            onClick={handleClick}
+            to="contact"
+            spy
+            hashSpy
+            smooth
+            delay={200}
+            duration={600}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
     </NavContainer>
   );
 };
@@ -225,6 +304,54 @@ const NavContainer = styled.header`
         color: var(--green);
       }
     }
+  }
+
+  .links {
+    position: fixed;
+    top: 0;
+    right: -2000px;
+    display: flex;
+    flex-direction: column;
+    width: 19rem;
+    background-color: var(--white);
+    min-height: 100vh;
+    margin: 0;
+    padding: 2rem 1.5rem 9.5rem;
+    transition: 0.5s all ease-in-out;
+    z-index: 2000;
+    box-shadow: -10px 0 20px -10px rgba(0, 0, 0, 0.1);
+
+    .nav-item {
+      list-style: none;
+      font-size: 1.25rem;
+      padding: 0.625rem 1.25rem;
+      color: var(--btn-hover);
+    }
+
+    .nav-item .active {
+      color: var(--green);
+    }
+
+    .nav-item.close {
+      display: flex;
+      align-items: center;
+      justify-content: end;
+
+      button {
+        background-color: transparent;
+        border: none;
+        padding: 0;
+        .x-mark {
+          color: var(--title);
+          font-size: var(--fs-title);
+        }
+      }
+    }
+  }
+
+  .links.active {
+    right: 0;
+    transition: 0.5s all ease-in-out;
   }
 `;
 
